334c6c861d8f7033dac9578db46b45fbcd38976d
Anselmo Pfeifer <anselmo.pfeifer@tecsinapse.com.br>
2016-08-14 23:40:02 -0400

    Finalizando o curso de vagrant na alura.

--- Vagrant-Puppet/manifests/bootstrap.sh
+++ Vagrant-Puppet/manifests/bootstrap.sh
@@ -1,17 +1,17 @@
 #!/bin/sh
+
 set -e -x
 
 if which puppet > /dev/null ; then
 	echo "Puppet is already installed"
 	exit 0
+	else
+	export DEBIAN_FRONTEND=noninteractive
+	wget -qO /tmp/puppetlabs-release-pc1-xenial.deb https://apt.puppetlabs.com/puppetlabs-release-pc1-xenial.deb
+	sudo dpkg -i /tmp/puppetlabs-release-pc1-xenial.deb
+	sudo rm /tmp/puppetlabs-release-pc1-xenial.deb
+	sudo apt-get update
+	echo "Installing puppet"
+	sudo apt-get install puppet-common puppet -y
+	echo "Puppet installed!"
 fi
-
-export DEBIAN_FRONTEND=noninteractive
-wget -qO /tmp/puppetlabs-release-precise.deb https://apt.puppetlabs.com/puppetlabs-release-precise.deb
-
-dpkg -i /tmp/puppetlabs-release-precise.deb
-rm /tmp/puppetlabs-release-precise.deb
-aptitude update
-echo Installing puppet
-aptitude install -y puppet
-echo "Puppet installed!"
--- Vagrant-Puppet/manifests/default.pp
+++ Vagrant-Puppet/manifests/default.pp
@@ -2,14 +2,71 @@ exec { 'apt-update':
   command => '/usr/bin/apt-get update'
 }
 package {
-    ["openjdk-8-jre","tomcat8"]:
-        ensure => installed,
-        require => Exec["apt-update"]
+  ["openjdk-8-jre","tomcat8", "mysql-server", "php", "apache2", "htop", "git"]:
+    ensure => installed,
+    require => Exec["apt-update"]
 }
 
-package { 'apache2':
-  require => Exec['apt-update'],
-  ensure => installed,
+service { "tomcat8":
+  ensure => running,
+  enable => true,
+  hasstatus => true,
+  hasrestart => true,
+  require => Package["tomcat8"]
+}
+exec{"download":
+  command => "/usr/bin/wget -q http://github.com/alura-cursos/provisionamento-com-vagrant-e-puppet/raw/master/manifests/vraptor-musicjungle.war -O /tmp/ROOT.war"
+}
+
+file_line { 'production':
+  file => "/etc/default/tomcat8",
+  line => "JAVA_OPTS=\"\$JAVA_OPTS -Dbr.com.caelum.vraptor.environment=production\"",
+  require => Package["tomcat8"],
+  notify => Service["tomcat8"]
+}
+define file_line($file, $line) {
+  exec { "/bin/echo '${line}' >> '${file}'":
+    unless => "/bin/grep -qFx '${line}' '${file}'"
+  }
+}
+service { "mysql":
+  ensure => running,
+  enable => true,
+  hasstatus => true,
+  hasrestart => true,
+  require => Package["mysql-server"]
+}
+exec { "create_database_musicjungle":
+  command => "mysqladmin -uroot create musicjungle",
+  unless => "mysql -u root musicjungle",
+  path => "/usr/bin",
+  require => Service["mysql"]
+}
+#exec { "create_database_musicjungle":
+#  command => 'mysql -uroot -e \"grant all on musicjungle.* to musicjungle@localhost identified by 'musicjungle';\",
+#  path => "/usr/bin",
+#  require => Service["mysql"]
+#}
+
+exec { 'remove_sites_enabled':
+  command => '/bin/rm -rf /etc/apache2/sites-enabled/000* /etc/apache2/sites-available/000*'
+}
+file { '/etc/apache2/sites-available/default.conf':
+  source => '/vagrant/manifests/default.conf',
+}
+file { '/etc/apache2/sites-enabled/default.conf':
+  ensure => 'link',
+  target => '/etc/apache2/sites-available/default.conf',
+}
+file { '/etc/apache2/mods-enabled/proxy.load':
+  ensure => 'link',
+  target => '/etc/apache2/mods-available/proxy.load',
+}
+file { '/etc/apache2/mods-enabled/proxy_http.load':
+  ensure => 'link',
+  target => '/etc/apache2/mods-available/proxy_http.load',
+  notify => Service["apache2"],
+  require => Package["apache2"],
 }
 service { 'apache2':
   ensure => running,
@@ -18,64 +75,61 @@ service { 'apache2':
   hasrestart => true,
   require => Package['apache2']
 }
-exec { 'remove_sites_enabled':
-  command => '/bin/rm -fr /etc/apache2/sites-enabled/000*'
+package { 'libapache2-mod-php':
+  require => Exec['apt-update'],
+  ensure => installed,
 }
 file { '/var/www/html/info.php':
   ensure => file,
   content => '<?php  phpinfo(); ?>',
   require => Package['apache2'],
 }
-file { '/etc/apache2/sites-available/default.conf':
-  source => '/vagrant/manifests/default.conf',
-}
-exec { 'link_sites':
-  command => '/bin/ln -s /etc/apache2/sites-available/default.conf /etc/apache2/sites-enabled/default.conf',
-  notify => Service["apache2"],
-}
 
-package { 'mysql-server':
-  require => Exec['apt-update'],
-  ensure => installed,
+exec { 'remove_app':
+  command => '/bin/cp /tmp/ROOT.war /var/lib/tomcat8/webapps/ROOT.war'
 }
-service { "mysql":
-    ensure => running,
-    enable => true,
-    hasstatus => true,
-    hasrestart => true,
-    require => Package["mysql-server"]
-}
-exec { "create_database_musicjungle":
-    command => "mysqladmin -uroot create musicjungle",
-    unless => "mysql -u root musicjungle",
-    path => "/usr/bin",
-    require => Service["mysql"]
-}
-
-#package { 'php':
-#  require => Exec['apt-update'],
-#  ensure => installed,
-#}
-#package { 'libapache2-mod-php':
-#  require => Exec['apt-update'],
-#  ensure => installed,
+#file { "/var/lib/tomcat8/webapps/ROOT.war":
+#  source => "/tmp/ROOT.war",
+#  ensure => file,
+#  owner => "tomcat8",
+#  group => "tomcat8",
+#  require => Package["tomcat8"],
+#  notify => Service["tomcat8"]
 #}
 
-service { "tomcat8":
-    ensure => running,
-    enable => true,
-    hasstatus => true,
-    hasrestart => true,
-    require => Package["tomcat8"]
-}
-exec{"download":
-    command => "/usr/bin/wget -q http://github.com/alura-cursos/provisionamento-com-vagrant-e-puppet/raw/master/manifests/vraptor-musicjungle.war -O /vagrant/manifests/ROOT.war"
-}
-file { "/var/lib/tomcat8/webapps/ROOT.war":
-    source => "/vagrant/manifests/ROOT.war",
-    owner => "tomcat8",
-    group => "tomcat8",
-    mode => 0644,
-    require => Package["tomcat8"],
-    notify => Service["tomcat8"]
-}
+#  file { '/etc/apache2/mods-enabled/userdir.load':
+#    ensure => 'link',
+#    target => '/etc/apache2/mods-available/userdir.load',
+#    notify => Service["apache2"],
+#    require => Package["apache2"],
+#  }
+#  file { '/etc/apache2/mods-enabled/userdir.conf':
+#    ensure => 'link',
+#    target => '/etc/apache2/mods-available/userdir.conf',
+#    notify => Service["apache2"],
+#    require => Package["apache2"],
+#  }
+#  exec { 'link_sites':
+#    command => '/bin/ln -s /etc/apache2/sites-available/default.conf /etc/apache2/sites-enabled/default.conf',
+#    notify => Service["apache2"],
+#  }
+#  exec { 'remove_sites_enabled':
+#    command => '/bin/rm -rf /etc/apache2/sites-enabled/000* /etc/apache2/sites-available/000*'
+#  }
+
+#exec { 'link_sites':
+#  command => '/bin/ln -s /etc/apache2/sites-available/default.conf /etc/apache2/sites-enabled/default.conf',
+#  notify => Service["apache2"],
+#}
+
+
+
+
+#
+#class absent_file_exec {
+#  exec { 'download':
+#    command => '/usr/bin/wget -q http://github.com/alura-cursos/provisionamento-com-vagrant-e-puppet/raw/master/manifests/vraptor-musicjungle.war -O /tmp/ROOT.war',
+#    creates => '/tmp/ROOT.war'
+#  }
+#}
+#
