4f9203d2c43239253229e54d7645e2e2f8cb08cf
Anselmo Pfeifer <anselmo.pfeifer@tecsinapse.com.br>
2016-08-15 17:31:25 -0400

    Finalizando o curso de vagrant na alura.

---
+++ Vagrant-Puppet/AWS-VagrantFile.rb
@@ -0,0 +1,31 @@
+Vagrant.configure(2) do |config|
+  config.vm.box = "hashicorp/precise32"
+  config.vm.box = "ubuntu_awss"
+  config.vm.box_url = "https://github.com/mitchellh/vagrant-aws/raw/master/dummy.box"
+
+  config.vm.provider :aws do |aws, override|
+    aws.access_key_id = "SEU_ACCESS_KEY_ID"
+    aws.secret_access_key = "SEU_SECRET_ACCESS_KEY"
+
+    aws.keypair_name = "devops"
+    aws.ami = "SEU_AMI_AQUI"
+    aws.security_groups = ['devops']
+
+    override.ssh.username = "ubuntu"
+    override.ssh.private_key_path = "devops.pem"
+  end
+
+  config.vm.define :web do |web_config|
+    web_config.vm.network "private_network", ip: "192.168.50.10"
+    web_config.vm.provision "shell", path: "manifests/bootstrap.sh"
+
+    web_config.vm.provision "puppet" do |puppet|
+    puppet.manifest_file = "web.pp"
+    end
+
+    web_config.vm.provider :aws do |aws|
+      aws.tags = { 'Name' => 'music-jungle'}
+    end
+  end
+
+end
--- Vagrant-Puppet/README.md
+++ Vagrant-Puppet/README.md
@@ -4,6 +4,8 @@
 - vagrant provision
 - vagrant provision --provision=aws
 - vagrant provision --provision-with puppet
+- vagrant plugin install vagrant-aws
+
 
 - sudo puppet apply /vagrant/manifests/default.pp
 - sudo puppet apply --test
